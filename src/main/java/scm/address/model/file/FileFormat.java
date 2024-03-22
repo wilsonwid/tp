7e59d66bc011bd0b450ff60fe6d6cc30529f4cbd
tahsinhasem <tahsinhasem@gmail.com>
2024-03-22 23:33:34 +0800

    Extracted out logic of checking for filetype from ImportCommand into FileFormat class

--- src/main/java/scm/address/model/file/FileFormat.java
+++ src/main/java/scm/address/model/file/FileFormat.java
@@ -10,6 +10,15 @@ public class FileFormat {
     public static final String MESSAGE_INVALID_FILE_FORMAT = "Invalid file format!";
     public static final String UNSUPPORTED_FILE_FORMAT = "Unsupported file format!";
 
+
+    /**
+     * Returns the file format of the {@code file}. Throws an {@code IllegalValueException} if the file format is not
+     * found, or is unsupported.
+     *
+     * @param file A File.
+     * @return The file format of the {@code file}.
+     * @throws IllegalValueException If the file format is not found, or is unsupported.
+     */
     public static String getFileFormat(File file) throws IllegalValueException {
         String fileName = file.getName();
         int index = fileName.lastIndexOf(".");
