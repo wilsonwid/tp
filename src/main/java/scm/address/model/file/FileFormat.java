0ab1943ff618ef98e60d754778e775b3b7e23138
tahsinhasem <tahsinhasem@gmail.com>
2024-03-22 23:27:28 +0800

    Added code to handle file formats

--- src/main/java/scm/address/model/file/FileFormat.java
+++ src/main/java/scm/address/model/file/FileFormat.java
@@ -1,4 +1,30 @@
 package scm.address.model.file;
 
+import scm.address.commons.exceptions.IllegalValueException;
+
+import java.io.File;
+
 public class FileFormat {
+    public static final String JSON_FILE = "json";
+    public static final String CSV_FILE = "csv";
+    public static final String MESSAGE_INVALID_FILE_FORMAT = "Invalid file format!";
+    public static final String UNSUPPORTED_FILE_FORMAT = "Unsupported file format!";
+
+    public static String getFileFormat(File file) throws IllegalValueException {
+        String fileName = file.getName();
+        int index = fileName.lastIndexOf(".");
+        if (index == -1) {
+            throw new IllegalValueException(MESSAGE_INVALID_FILE_FORMAT);
+        }
+
+        String tempEnd = fileName.substring(index + 1).toLowerCase();
+        switch (tempEnd) {
+        case "json":
+            return JSON_FILE;
+        case "csv":
+            return CSV_FILE;
+        default:
+            throw new IllegalValueException(UNSUPPORTED_FILE_FORMAT);
+        }
+    }
 }
