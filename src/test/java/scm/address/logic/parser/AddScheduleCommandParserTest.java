f5c3dcf9e17463078237fcc55fbf14ace403fa2f
Chua Zi Long <e0957913@u.nus.edu>
2024-03-28 22:23:07 +0800

    Fix code to match checkstyle reqs

--- src/test/java/scm/address/logic/parser/AddScheduleCommandParserTest.java
+++ src/test/java/scm/address/logic/parser/AddScheduleCommandParserTest.java
@@ -2,8 +2,8 @@ package scm.address.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
