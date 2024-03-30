fe3a704ddffe6a47e8b398b060039939c026322c
Chua Zi Long <e0957913@u.nus.edu>
2024-03-30 18:26:54 +0800

    Add Javadocs to the new date time comparison class

--- src/main/java/scm/address/model/util/DateTimeComparison.java
+++ src/main/java/scm/address/model/util/DateTimeComparison.java
@@ -4,10 +4,21 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+/**
+ * Utility class for comparing date and time values.
+ */
 public class DateTimeComparison {
     public DateTimeComparison() {
 
     }
+
+    /**
+     * Checks if the first date and time is before the second date and time.
+     *
+     * @param datetimeStr1 A {@code String} representing the first date and time.
+     * @param datetimeStr2 A {@code String} representing the second date and time.
+     * @return {@code true} if the first date and time is before the second date and time, {@code false} otherwise.
+     */
     public boolean isFirstDateTimeBeforeSecond(String datetimeStr1, String datetimeStr2) {
         DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 
