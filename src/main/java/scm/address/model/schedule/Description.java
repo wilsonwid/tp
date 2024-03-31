a7b4acf99c60995cf6632fbe0b913b47e4287eb2
Wilson W <wilsonwidyadhana681@gmail.com>
2024-03-31 12:50:32 +0800

    Add javadoc and validation checks

--- src/main/java/scm/address/model/schedule/Description.java
+++ src/main/java/scm/address/model/schedule/Description.java
@@ -4,11 +4,39 @@ package scm.address.model.schedule;
  * Represents a Schedule's description in the address book.
  */
 public class Description {
-    final String description;
+    /** Informs the user of the constraints for valid descriptions. **/
+    public static final String MESSAGE_CONSTRAINTS = "Descriptions should be alphanumeric and cannot be empty. They can contain a space only after the first character";
+
+    /** The regular expression to be used for determinig whether a Description is valid. **/
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    private final String description;
+
+    /** Constructs a new Description instance.
+     *
+     * @param description The description to be added.
+     */
     public Description(String description) {
         this.description = description;
     }
 
+    /**
+     * Returns whether the given {@code description} is a valid description.
+     *
+     * @param description The description to be checked.
+     * @return A boolean value.
+     */
+    public static boolean isValidDescription(String description) {
+        return description.matches(VALIDATION_REGEX);
+    }
+
+    /**
+     * Returns whether this instance is equal to another.
+     * Two instances of Descriptions are equal only if their descriptions are equal.
+     *
+     * @param other The other Object to be compared against.
+     * @return A boolean value.
+     */
     @Override
     public boolean equals(Object other) {
         if (this == other) {
@@ -23,6 +51,11 @@ public class Description {
         return false;
     }
 
+    /**
+     * Returns the String representation of this object, which is its description.
+     *
+     * @return A String.
+     */
     @Override
     public String toString() {
         return description;
--- src/main/java/scm/address/model/schedule/Title.java
+++ src/main/java/scm/address/model/schedule/Title.java
@@ -4,8 +4,19 @@ package scm.address.model.schedule;
  * Represents a Schedule's title in the address book.
  */
 public class Title {
-    public static final String MESSAGE_CONSTRAINTS = "Titles should be alphanumeric and cannot be empty.";
+    /** Informs the user of the constriants for valid titles. **/
+    public static final String MESSAGE_CONSTRAINTS = "Titles should be alphanumeric and cannot be empty. They can contain a space only after the first character.";
+
+    /** The regular expression to be used for determining whether a Title is valid. **/
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
     private final String title;
+
+    /**
+     * Constructs a new Title instance.
+     *
+     * @param title The title to be added.
+     */
     public Title(String title) {
         this.title = title;
     }
@@ -16,15 +27,17 @@ public class Title {
      * @param title The title to be checked.
      * @return A boolean value.
      */
-    public boolean isValidTitle(String title) {
-
+    public static boolean isValidTitle(String title) {
+        return title.matches(VALIDATION_REGEX);
     }
 
-    @Override
-    public String toString() {
-        return title;
-    }
-
+    /**
+     * Returns whether this instance is equal to another.
+     * Two instances of Titles are equal only if the title equals the other title.
+     *
+     * @param other The other Object to be compared against.
+     * @return A boolean value.
+     */
     @Override
     public boolean equals(Object other) {
         if (this == other) {
@@ -37,5 +50,15 @@ public class Title {
         }
 
         return false;
+    }
+
+    /**
+     * Returns the String representation of this object, which is its title.
+     *
+     * @return A String.
+     */
+    @Override
+    public String toString() {
+        return title;
     }
 }
