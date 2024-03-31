6150eabbd501f07d3da7a5fff5b9c1519356735f
Wilson W <wilsonwidyadhana681@gmail.com>
2024-03-31 19:43:34 +0800

    Add javadoc comments

--- src/test/java/scm/address/testutil/EditScheduleDescriptorBuilder.java
+++ src/test/java/scm/address/testutil/EditScheduleDescriptorBuilder.java
@@ -7,6 +7,9 @@ import scm.address.model.schedule.Title;
 
 import java.time.LocalDateTime;
 
+/**
+ * A utility class to help with building EditScheduleDescriptor objects.
+ */
 public class EditScheduleDescriptorBuilder {
     private EditScheduleDescriptor descriptor;
 
@@ -18,6 +21,11 @@ public class EditScheduleDescriptorBuilder {
         this.descriptor = descriptor;
     }
 
+    /**
+     * Constructs an EditScheduleDescriptorBuilder given the {@code schedule}.
+     *
+     * @param schedule Source Schedule.
+     */
     public EditScheduleDescriptorBuilder(Schedule schedule) {
         this.descriptor = new EditScheduleDescriptor();
         this.descriptor.setTitle(schedule.getTitle());
@@ -26,28 +34,57 @@ public class EditScheduleDescriptorBuilder {
         this.descriptor.setEndDateTime(schedule.getEndDateTime());
     }
 
+    /**
+     * Sets the {@code Title} of the {@code EditScheduleDescriptor} we are building.
+     *
+     * @param title Title to be used.
+     * @return An {@link EditScheduleDescriptorBuilder}.
+     */
     public EditScheduleDescriptorBuilder withTitle(String title) {
         this.descriptor.setTitle(new Title(title));
         return this;
     }
 
+    /**
+     * Sets the {@code Description} of the {@code EditScheduleDescriptor} we are building.
+     *
+     * @param description Description to be used.
+     * @return An {@link EditScheduleDescriptorBuilder}.
+     */
     public EditScheduleDescriptorBuilder withDescription(String description) {
         this.descriptor.setDescription(new Description(description));
         return this;
     }
 
+    /**
+     * Sets the {@code startDateTime} of the {@code EditScheduleDescriptor} we are building.
+     *
+     * @param startStringTime  Starting time (in String) to be used.
+     * @return An {@link EditScheduleDescriptorBuilder}.
+     */
     public EditScheduleDescriptorBuilder withStartDateTime(String startStringTime) {
         this.descriptor.setStartDateTime(LocalDateTime
                 .parse(startStringTime, Schedule.DATE_TIME_FORMATTER));
         return this;
     }
 
+    /**
+     * Sets the {@code endDateTime} of the {@code EditScheduleDescriptor} we are building.
+     *
+     * @param endDateTime Ending time (in String) to be used.
+     * @return An {@link EditScheduleDescriptorBuilder}.
+     */
     public EditScheduleDescriptorBuilder withEndDateTime(String endDateTime) {
         this.descriptor.setEndDateTime(LocalDateTime
                 .parse(endDateTime, Schedule.DATE_TIME_FORMATTER));
         return this;
     }
 
+    /**
+     * Builds the {@code EditScheduleDescriptor}.
+     *
+     * @return An {@link EditScheduleDescriptor}.
+     */
     public EditScheduleDescriptor build() {
         return this.descriptor;
     }
