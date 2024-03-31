067c8d9738af877f1d2c27684cfb1e26ece3a23c
Chua Zi Long <e0957913@u.nus.edu>
2024-03-31 21:17:43 +0800

    Fix code to comply with checkstyle

--- src/test/java/scm/address/logic/commands/AddScheduleCommandTest.java
+++ src/test/java/scm/address/logic/commands/AddScheduleCommandTest.java
@@ -1,5 +1,11 @@
 package scm.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -10,12 +16,6 @@ import scm.address.model.ModelManager;
 import scm.address.model.schedule.Description;
 import scm.address.model.schedule.Schedule;
 import scm.address.model.schedule.Title;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AddScheduleCommandTest {
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
