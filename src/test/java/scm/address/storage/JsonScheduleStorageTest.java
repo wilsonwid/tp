6d19dae83e9dc03532ef4d9db38760e81a9cb46e
Wilson W <wilsonwidyadhana681@gmail.com>
2024-04-02 13:20:12 +0800

    Add JsonScheduleStorageTest

--- src/test/java/scm/address/storage/JsonScheduleStorageTest.java
+++ src/test/java/scm/address/storage/JsonScheduleStorageTest.java
@@ -1,7 +1,9 @@
 package scm.address.storage;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static scm.address.testutil.Assert.assertThrows;
+import static scm.address.testutil.TypicalSchedules.getTypicalScheduleList;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,6 +19,11 @@ public class JsonScheduleStorageTest {
     private static final Path NON_EXISTENT_FILE = TEST_DATA_FOLDER.resolve("nonExistentFile.json");
 
     private static final Path NON_JSON_FILE = TEST_DATA_FOLDER.resolve("notJsonFormatScheduleList.json");
+
+    private static final Path INVALID_JSON_FILE = TEST_DATA_FOLDER.resolve("invalidSchedulesScheduleList.json");
+
+    private static final Path TYPICAL_SCHEDULE_FILE = TEST_DATA_FOLDER.resolve("typicalSchedulesScheduleList.json");
+
     @TempDir
     public Path testFolder;
 
@@ -36,5 +43,24 @@ public class JsonScheduleStorageTest {
     public void read_notJsonFormat_exceptionThrown() {
         JsonScheduleStorage storage = new JsonScheduleStorage(NON_JSON_FILE);
         assertThrows(DataLoadingException.class, () -> storage.readScheduleList());
+    }
+
+    @Test
+    public void read_invalidJsonFormat_exceptionThrown() {
+        JsonScheduleStorage storage = new JsonScheduleStorage(INVALID_JSON_FILE);
+        assertThrows(DataLoadingException.class, () -> storage.readScheduleList());
+    }
+
+    @Test
+    public void read_typicalScheduleList_success() throws Exception {
+        JsonScheduleStorage storage = new JsonScheduleStorage(TYPICAL_SCHEDULE_FILE);
+        assertEquals(storage.readScheduleList().get(), getTypicalScheduleList());
+    }
+
+    @Test
+    public void save_typicalScheduleList_success() throws Exception {
+        JsonScheduleStorage storage = new JsonScheduleStorage(TYPICAL_SCHEDULE_FILE);
+        storage.saveScheduleList(getTypicalScheduleList());
+        assertEquals(storage, storage);
     }
 }
