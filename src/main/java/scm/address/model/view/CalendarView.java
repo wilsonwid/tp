12891d25328220d1a89bb7aff8635f329168a458
Chua Zi Long <e0957913@u.nus.edu>
2024-04-02 16:40:47 +0800

    Fix code to match checkstyle

--- src/main/java/scm/address/model/view/CalendarView.java
+++ src/main/java/scm/address/model/view/CalendarView.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-
 import scm.address.model.Model;
 import scm.address.model.schedule.Schedule;
 
