caad2391784006c57a8ba71b48aedb356e62f65e
Wilson W <wilsonwidyadhana681@gmail.com>
2024-04-02 12:39:21 +0800

    Fix checkstyle errors

--- src/test/java/scm/address/model/ScheduleListTest.java
+++ src/test/java/scm/address/model/ScheduleListTest.java
@@ -1,11 +1,22 @@
 package scm.address.model;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Iterator;
+
+import org.junit.jupiter.api.Test;
+
+import scm.address.model.schedule.Schedule;
+
 public class ScheduleListTest {
+
+    @Test
+    public void returnIterator() {
+        ScheduleList schedules = new ScheduleList();
+        Iterator<Schedule> iterator = schedules.iterator();
+        assertTrue(iterator.equals(iterator));
+    }
     @Test
     public void equals() {
         ScheduleList schedules = new ScheduleList();
