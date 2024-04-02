08102eb209a866d12fe2ed8557f8bfa2e273f82d
Wilson W <wilsonwidyadhana681@gmail.com>
2024-04-02 12:15:25 +0800

    Add more tests

---
+++ src/test/java/scm/address/model/schedule/ScheduleTest.java
@@ -0,0 +1,67 @@
+package scm.address.model.schedule;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static scm.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import scm.address.testutil.ScheduleBuilder;
+
+import java.time.LocalDateTime;
+
+
+public class ScheduleTest {
+    @Test
+    public void constructor_createSchedule_success() {
+        Title title = new Title("Meeting");
+        Description description = new Description("Project meeting");
+        LocalDateTime startDateTime = LocalDateTime.parse("2024-03-10 16:00", Schedule.DATE_TIME_FORMATTER);
+        LocalDateTime endDateTime = LocalDateTime.parse("2024-03-10 18:00", Schedule.DATE_TIME_FORMATTER);
+        Schedule schedule = new Schedule(title, description, startDateTime, endDateTime);
+        assertTrue(schedule instanceof Schedule);
+    }
+
+    @Test
+    public void equals() {
+        Schedule schedule = new ScheduleBuilder()
+                .withTitle("Meeting")
+                .withDescription("Project meeting")
+                .withStartDateTime("2024-03-10 16:00")
+                .withEndDateTime("2024-03-10 18:00")
+                .build();
+
+        Schedule otherSchedule = new ScheduleBuilder(schedule).build();
+
+        assertTrue(schedule.equals(otherSchedule));
+
+        otherSchedule = new ScheduleBuilder(schedule)
+                .withTitle("Project meeting")
+                .build();
+
+        assertFalse(schedule.equals(otherSchedule));
+
+        otherSchedule = new ScheduleBuilder(schedule)
+                .withDescription("Another project meeting")
+                .build();
+
+        assertFalse(schedule.equals(otherSchedule));
+
+        otherSchedule = new ScheduleBuilder(schedule)
+                .withStartDateTime("2024-03-11 16:00")
+                .build();
+
+        assertFalse(schedule.equals(otherSchedule));
+
+        otherSchedule = new ScheduleBuilder(schedule)
+                .withEndDateTime("2024-03-11 18:00")
+                .build();
+
+        assertFalse(schedule.equals(otherSchedule));
+    }
+
+    @Test
+    public void constructor_nullField_failure() {
+        assertThrows(NullPointerException.class, () -> new Schedule((Title) null, (Description) null,
+                (LocalDateTime) null, (LocalDateTime) null));
+    }
+}
