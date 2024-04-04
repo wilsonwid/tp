6ee6a7fb09043530f22eb7eba49325725e3148b8
tahsinhasem <tahsinhasem@gmail.com>
2024-04-04 22:02:53 +0800

    Add a LightTheme and a DarkTheme implementing the Theme class

---
+++ src/main/java/scm/address/model/theme/DarkTheme.java
@@ -0,0 +1,25 @@
+package scm.address.model.theme;
+
+/**
+ * Represents the Dark Theme of the application.
+ */
+class DarkTheme implements Theme {
+    private static final String DARK_THEME_NAME = "dark";
+    private static final String DARK_THEME_CSS_PATH = "/view/DarkTheme.css";
+    private static final String DARK_THEME_EXTENSIONS_CSS_PATH = "/view/Extensions.css";
+
+    @Override
+    public String getThemeName() {
+        return DARK_THEME_NAME;
+    }
+
+    @Override
+    public String getThemeCssPath() {
+        return DARK_THEME_CSS_PATH;
+    }
+
+    @Override
+    public String getThemeExtensionsCssPath() {
+        return DARK_THEME_EXTENSIONS_CSS_PATH;
+    }
+}
