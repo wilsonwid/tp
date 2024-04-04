6e8a2ef6961eab9f39f2e3013f128fab4279c74c
tahsinhasem <tahsinhasem@gmail.com>
2024-04-05 06:08:00 +0800

    Fix checkstyle error

--- src/test/java/scm/address/model/theme/ThemeCollectionTest.java
+++ src/test/java/scm/address/model/theme/ThemeCollectionTest.java
@@ -1,9 +1,9 @@
 package scm.address.model.theme;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 public class ThemeCollectionTest {
 
