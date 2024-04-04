ac3725ab358cf80e379d541c70aec8face0de3a3
tahsinhasem <tahsinhasem@gmail.com>
2024-04-04 14:54:13 +0800

    Add Message Useage for ThemeCommand.java

--- src/main/java/scm/address/logic/commands/ThemeCommand.java
+++ src/main/java/scm/address/logic/commands/ThemeCommand.java
@@ -7,6 +7,11 @@ public class ThemeCommand extends Command{
 
     public static final String COMMAND_WORD = "theme";
     public static final String MESSAGE_SUCCESS = "Theme changed";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Changes the theme of the application.\n"
+            + "Parameters: "
+            + "THEME\n"
+            + "Example: " + COMMAND_WORD + " light";
+
 
     public ThemeCommand() {
     }
